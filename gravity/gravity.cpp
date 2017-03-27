89bf62e706872e4470d0331740bab2dbfc20c8eb
Jean Louw <me@jlouw.co.za>
2017-03-27 17:00:47 +0200

    random number generator

--- gravity/gravity.cpp
+++ gravity/gravity.cpp
@@ -2,11 +2,29 @@
 //
 
 #include "stdafx.h"
+#include <iostream>;
+#include <random>;
+
 
 
 int main()
 {
+	std::random_device rd;
+	std::mt19937 mersenne(rd());
+
+	for (int x = 0; x < 52; x++)
+	{
+		std::cout << mersenne() << "\t";
+
+		if ((x + 1) % 4 == 0)
+		{
+			std::cout << "\n";
+		}
+	}
 	// comment
+	int z = 0;
+	std::cin >> z;
+
     return 0;
 }
 
